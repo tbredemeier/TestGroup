f7e0ee75abe873e6d55cec95e54337ccd4ecd669
Tom Bredemeier <tbredemeier@barrington220.org>
2024-04-12 09:33:57 -0500

    Initial Commit

---
+++ TestGroup/TestGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGroupApp.swift
+//  TestGroup
+//
+//  Created by Tom Bredemeier on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
